f9418e00903bf9baa4fb767d808b06fa32fb2d09
anuradha <anuradha.kadurugasyaya@gmail.com>
2024-01-20 13:41:31 +0800

    Initial commit

---
+++ src/main/java/com/govtech/restaurantdecider/RestaurantDeciderApplication.java
@@ -0,0 +1,13 @@
+package com.govtech.restaurantdecider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantDeciderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantDeciderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/govtech/restaurantdecider/RestaurantDeciderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govtech.restaurantdecider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantDeciderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
